a54d5f1fba3a27ff8d257ddbc683a5a24363d015
davidp <davidp@arcbox1116.zurich.architonic.net>
2015-07-09 09:42:43 +0200

    Cleanup & Fixes

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-angular.module( 'pplibdataanalyzer_frontend', [
+angular.module('pplibdataanalyzer_frontend', [
   'pplibdataanalyzer_frontend.home',
   'pplibdataanalyzer_frontend.login',
   'pplibdataanalyzer_frontend.signup',
   'pplibdataanalyzer_frontend.search',
-    'pplibdataanalyzer_frontend.contact',
-    'pplibdataanalyzer_frontend.about',
+  'pplibdataanalyzer_frontend.contact',
+  'pplibdataanalyzer_frontend.about',
   'angular-jwt',
   'angular-storage',
-'ui.bootstrap',
+  'ui.bootstrap',
 ])
-.config( function myAppConfig ($urlRouterProvider, jwtInterceptorProvider, $httpProvider) {
-  $urlRouterProvider.otherwise('/');
+        .config(function myAppConfig($urlRouterProvider, jwtInterceptorProvider, $httpProvider) {
+          $urlRouterProvider.otherwise('/');
 
-  jwtInterceptorProvider.tokenGetter = function(store) {
-    return store.get('jwt');
-  }
+          jwtInterceptorProvider.tokenGetter = function (store) {
+            return store.get('jwt');
+          }
 
-  $httpProvider.interceptors.push('jwtInterceptor');
-    
+          $httpProvider.interceptors.push('jwtInterceptor');
 
-})
-.run(function($rootScope, $state, store, jwtHelper) {
-  $rootScope.$on('$stateChangeStart', function(e, to) {
-    if (to.data && to.data.requiresLogin) {
-      if (!store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'))) {
-        e.preventDefault();
-        $state.go('login');
-      }
-    }
-  });
-})
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location, store) {
-    
-    $scope.loggedin = false;
-    if(store.get('jwt')) {
-     $scope.loggedin = true;   
-    }
 
-});
+        })
+        .run(function ($rootScope, $state, store, jwtHelper) {
+          $rootScope.$on('$stateChangeStart', function (e, to) {
+            if (to.data && to.data.requiresLogin) {
+              if (!store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'))) {
+                e.preventDefault();
+                $state.go('login');
+              }
+            }
+          });
+        })
+        .controller('AppCtrl', function AppCtrl($scope, store) {
+
+          $scope.loggedin = false;
+          if (store.get('jwt')) {
+            $scope.loggedin = true;
+          }
+
+        });
--- login/login.js
+++ login/login.js
@@ -1,36 +1,37 @@
-angular.module( 'pplibdataanalyzer_frontend.login', [
+angular.module('pplibdataanalyzer_frontend.login', [
   'ui.router',
   'angular-storage'
 ])
-.config(function($stateProvider) {
-  $stateProvider.state('login', {
-    url: '/login',
-    controller: 'LoginCtrl',
-    templateUrl: 'login/login.html'
-  });
-})
-.controller( 'LoginCtrl', function LoginController( $scope, $http, store, $state) {
+        .config(function ($stateProvider) {
+          $stateProvider.state('login', {
+            url: '/login',
+            controller: 'LoginCtrl',
+            templateUrl: 'login/login.html'
+          });
+        })
+        .controller('LoginCtrl', function LoginController($scope, $http, store, $state) {
 
-  $scope.user = {};
+          $scope.user = {};
 
-  $scope.login = function() {
-    $http({
-      url: '/users/getJWT',
-      method: 'POST',
-      data: $scope.user
-    }).then(function(response) {
-        if(response.data.length == 0) {
-            alert("login failed");
-        } else {
-            store.set('jwt', response.data);
-            window.location.reload(false);
-            $state.go('search');  
-        }
+          $scope.login = function () {
+            $http({
+              url: '/users/getJWT',
+              method: 'POST',
+              data: $scope.user
+            }).then(function (response) {
+              if (response.data.length === 0) {
+                alert("login failed");
+                $state.go('login');
+              } else {
+                store.set('jwt', response.data);
+                window.location.reload(false);
+                $state.go('search');
+              }
 
-        
-    }, function(error) {
-      //alert(error.data);
-    });
-  }
 
-});
+            }, function (error) {
+              //alert(error.data);
+            });
+          }
+
+        });
